eecdcaac43a5136a25d32499833ea0d8e8927dec
jorgetxb <neopixel.studio@outlook.com>
2023-07-03 17:39:14 -0500

    version 0.16

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,37 @@ var images = [
   "./img/gallery/imagen(2).jpg",
   "./img/gallery/imagen(3).jpg",
   "./img/gallery/imagen(4).jpg",
-  "./img/gallery/imagen(5).jpg"
+  "./img/gallery/imagen(5).jpg",
+  "./img/gallery/imagen(6).jpg",
+  "./img/gallery/imagen(7).jpg",
+  "./img/gallery/imagen(8).jpg",
+  "./img/gallery/imagen(9).jpg",
+  "./img/gallery/imagen(10).jpg",
+  "./img/gallery/imagen(11).jpg",
+  "./img/gallery/imagen(12).jpg",
+  "./img/gallery/imagen(13).jpg",
+  "./img/gallery/imagen(14).jpg",
+  "./img/gallery/imagen(15).jpg",
+  "./img/gallery/imagen(16).jpg",
+  "./img/gallery/imagen(17).jpg",
+  "./img/gallery/imagen(18).jpg",
+  "./img/gallery/imagen(19).jpg",
+  "./img/gallery/imagen(20).jpg",
+  "./img/gallery/imagen(21).jpg",
+  "./img/gallery/imagen(22).jpg",
+  "./img/gallery/imagen(23).jpg",
+  "./img/gallery/imagen(24).jpg",
+  "./img/gallery/imagen(25).jpg",
+  "./img/gallery/imagen(26).jpg",
+  "./img/gallery/imagen(27).jpg",
+  "./img/gallery/imagen(28).jpg",
+  "./img/gallery/imagen(29).jpg",
+  "./img/gallery/imagen(30).jpg",
+  "./img/gallery/imagen(31).jpg",
+  "./img/gallery/imagen(32).jpg",
+  "./img/gallery/imagen(33).jpg",
+  "./img/gallery/imagen(34).jpg",
+  "./img/gallery/imagen(35).jpg"
 ];
 
 var currentImage = 0;
@@ -12,9 +42,15 @@ var gallery = document.getElementById("content-6");
 function changeImage() {
   var img = new Image();
   img.src = images[currentImage];
+  img.className = "abus_photo";
 
   img.onload = function () {
     gallery.appendChild(img);
+    var imgs = document.getElementsByTagName('img'); // Obtener todos los elementos img dentro del div
+    var cantidadImagenes = imgs.length; // Obtener la cantidad de imágenes
+    if (cantidadImagenes > 2 ) {
+      document.getElementsByClassName("abus_photo")[0].remove();
+    }
   };
   // Incrementa el índice de la imagen actual
   currentImage++;
@@ -35,6 +71,8 @@ function abus_hide() {
   document.getElementById("hide").style.display = "none";
   document.getElementById("show").style.display = "block";
 
+  document.getElementById("brightness_photo").style.display = "none";
+
   opacity -= 0.1;
   document.querySelectorAll(".abus_text")[0].style.opacity = opacity;
   document.querySelectorAll(".abus_text")[1].style.opacity = opacity;
@@ -48,6 +86,8 @@ function abus_show() {
 
   document.getElementById("hide").style.display = "block";
   document.getElementById("show").style.display = "none";
+
+  document.getElementById("brightness_photo").style.display = "block";
   
   document.querySelectorAll(".abus_text")[0].style.opacity = "1";
   document.querySelectorAll(".abus_text")[1].style.opacity = "1";
